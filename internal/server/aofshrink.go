2b8a03bac4a67febfa703ecfa73ad6aa67fed57f
Alex Roitman <aroitman@housecanary.com>
2020-07-14 11:08:48 -0700

    Refactor/cleanup aof shrink.

--- internal/server/aofshrink.go
+++ internal/server/aofshrink.go
@@ -13,147 +13,131 @@ import (
 
 var errAOFAlreadyShrinking = errors.New("aof already shrinking")
 var errAOFDisabled = errors.New("aof disabled")
-var errAOFFailed = errors.New("aof failed")
+var errAOFShrinkFailed = errors.New("aof shrink failed")
+
+func writeResp(dst io.Writer, messages [][]string) error {
+	var buf []byte
+	for _, words := range messages {
+		for _, word := range words {
+			buf = append(buf, '$')
+			buf = append(buf, strconv.FormatInt(int64(len(word)), 10)...)
+			buf = append(buf, '\r', '\n')
+			buf = append(buf, word...)
+			buf = append(buf, '\r', '\n')
+		}
+	}
+	_, err := dst.Write(buf)
+	return err
+}
 
 func (server *Server) cmdAOFShrink() error {
 	if server.aof == nil {
 		return errAOFDisabled
 	}
-	start := time.Now()
-	ul := server.WriterLock()
-	if server.shrinking {
-		ul()
+
+	alreadyShrinking := func() bool {
+		defer server.WriterLock()()
+		if server.shrinking {
+			return true
+		} else {
+			server.shrinking = true
+			return false
+		}
+	}()
+	if alreadyShrinking {
 		return errAOFAlreadyShrinking
 	}
-	server.shrinking = true
-	server.shrinklog = nil
-	ul()
 
+	start := time.Now()
 	defer func() {
-		ul := server.WriterLock()
+		defer server.WriterLock()()
 		server.shrinking = false
 		server.shrinklog = nil
-		ul()
-		log.Infof("aof shrink ended %v", time.Now().Sub(start))
+		log.Infof("aof shrink finished %v", time.Now().Sub(start))
 	}()
 
-	// TODO: worry about ongoing replication that might be going over
-	// the part we're about to truncate
-
-	dst, err := os.Create(core.AppendFileName + "-shrink")
+	shrunkName := core.AppendFileName + "-shrink"
+	dst, err := os.Create(shrunkName)
 	if err != nil {
-		log.Errorf("Failed creating -shrink file: %v", err)
-		return errAOFFailed
+		log.Errorf("Failed creating shrunk file: %v", err)
+		return errAOFShrinkFailed
 	}
 
 	// Write "savesnapshot ID" at the top of the new file
-	var aofbuf []byte
-	values := []string{"SAVESNAPSHOT", server.snapshotMeta._idstr}
-	aofbuf = append(aofbuf, '*')
-	aofbuf = append(aofbuf, strconv.FormatInt(int64(len(values)), 10)...)
-	aofbuf = append(aofbuf, '\r', '\n')
-	for _, value := range values {
-		aofbuf = append(aofbuf, '$')
-		aofbuf = append(aofbuf, strconv.FormatInt(int64(len(value)), 10)...)
-		aofbuf = append(aofbuf, '\r', '\n')
-		aofbuf = append(aofbuf, value...)
-		aofbuf = append(aofbuf, '\r', '\n')
-	}
-	if _, err := dst.Write(aofbuf); err != nil {
-		log.Errorf("Failed writing header for -shrink file: %v", err)
-		return errAOFFailed
+	header := [][]string{{"SAVESNAPSHOT", server.snapshotMeta._idstr}}
+	if err := writeResp(dst, header); err != nil {
+		log.Errorf("Failed writing header for shrunk file: %v", err)
+		return errAOFShrinkFailed
 	}
 	newOffset, err := dst.Seek(0, io.SeekCurrent)
 	if err != nil {
-		log.Errorf("Failed seeking in -shrink file: %v", err)
-		return errAOFFailed
+		log.Errorf("Failed seeking in shrunk file: %v", err)
+		return errAOFShrinkFailed
 	}
-
 	src, err := os.Open(server.aof.Name())
 	if err != nil {
 		log.Errorf("Failed opening existing AOF file: %v", err)
-		return errAOFFailed
+		return errAOFShrinkFailed
 	}
 	if _, err := src.Seek(server.snapshotMeta._offset, io.SeekStart); err != nil {
 		log.Errorf("Failed seeking in existing AOF file: %v", err)
-		return errAOFFailed
+		return errAOFShrinkFailed
 	}
-
 	if _, err := io.Copy(dst, src); err != nil {
 		log.Errorf("Failed copying data: %v", err)
-		return errAOFFailed
+		return errAOFShrinkFailed
 	}
-
 	if err := dst.Sync(); err != nil {
-		log.Errorf("Failed syncing -shrink file: %v", err)
-		return errAOFFailed
+		log.Errorf("Failed syncing shrunk file: %v", err)
+		return errAOFShrinkFailed
 	}
 
 	defer server.WriterLock()()
 	// flush the aof buffer
 	server.flushAOF(false)
-	aofbuf = aofbuf[:0]
-	for _, values := range server.shrinklog {
-		// append the values to the aof buffer
-		aofbuf = append(aofbuf, '*')
-		aofbuf = append(aofbuf, strconv.FormatInt(int64(len(values)), 10)...)
-		aofbuf = append(aofbuf, '\r', '\n')
-		for _, value := range values {
-			aofbuf = append(aofbuf, '$')
-			aofbuf = append(aofbuf, strconv.FormatInt(int64(len(value)), 10)...)
-			aofbuf = append(aofbuf, '\r', '\n')
-			aofbuf = append(aofbuf, value...)
-			aofbuf = append(aofbuf, '\r', '\n')
-		}
-	}
-	if _, err := dst.Write(aofbuf); err != nil {
-		log.Errorf("Failed writing shrink log file: %v", err)
-		return errAOFFailed
+	// dump all records from shrinklog
+	if err := writeResp(dst, server.shrinklog); err != nil {
+		log.Errorf("Failed dumping shrink log records: %v", err)
+		return errAOFShrinkFailed
 	}
 	if err := dst.Sync(); err != nil {
-		log.Errorf("Failed syncing -shrink file: %v", err)
-		return errAOFFailed
+		log.Errorf("Failed syncing shrunk file: %v", err)
+		return errAOFShrinkFailed
 	}
 
-	// we now have a shrunken aof file that is fully in-sync with
-	// the current dataset. let's swap out the on disk files and
-	// point to the new file.
-
-	// anything below this point is unrecoverable. just log and exit process
-	// back up the live aof, just in case of fatal error
+	// we now have a shrunken aof file that is fully in-sync with the current state.
+	// let's swap out the on disk files and point to the new file.
 	if err := server.aof.Close(); err != nil {
-		log.Fatalf("shrink live aof close fatal operation: %v", err)
-		return errAOFFailed
+		log.Errorf("Failed closing AOF file: %v", err)
+		return errAOFShrinkFailed
 	}
 	if err := dst.Close(); err != nil {
-		log.Fatalf("shrink new aof close fatal operation: %v", err)
-		return errAOFFailed
+		log.Errorf("Failed closing shrunk file: %v", err)
+		return errAOFShrinkFailed
 	}
 	if err := os.Rename(core.AppendFileName, core.AppendFileName+"-bak"); err != nil {
-		log.Fatalf("shrink backup fatal operation: %v", err)
-		return errAOFFailed
+		log.Errorf("Failed renaming AOF file: %v", err)
+		return errAOFShrinkFailed
 	}
-	if err := os.Rename(core.AppendFileName+"-shrink", core.AppendFileName); err != nil {
-		log.Fatalf("shrink rename fatal operation: %v", err)
-		return errAOFFailed
+	if err := os.Rename(shrunkName, core.AppendFileName); err != nil {
+		log.Errorf("Failed renaming shrunk file: %v", err)
+		return errAOFShrinkFailed
 	}
-	server.aof, err = os.OpenFile(core.AppendFileName, os.O_CREATE|os.O_RDWR, 0600)
-	if err != nil {
-		log.Fatalf("shrink openfile fatal operation: %v", err)
-		return errAOFFailed
+	if server.aof, err = os.OpenFile(core.AppendFileName, os.O_CREATE|os.O_RDWR, 0600); err != nil {
+		log.Errorf("Failed opening AOF file: %v", err)
+		return errAOFShrinkFailed
 	}
-	server.aofsz, err = server.aof.Seek(0, 2)
-	if err != nil {
-		log.Fatalf("shrink seek end fatal operation: %v", err)
-		return errAOFFailed
+	if server.aofsz, err = server.aof.Seek(0, 2); err != nil {
+		log.Errorf("Failed seeking in AOF file: %v", err)
+		return errAOFShrinkFailed
 	}
 	server.snapshotMeta._offset = newOffset
 	if err := server.snapshotMeta.save(); err != nil {
 		log.Errorf("Failed saving snapshot meta: %v", err)
-		return errAOFFailed
+		return errAOFShrinkFailed
 	}
 
-	os.Remove(core.AppendFileName + "-bak") // ignore error
+	os.Remove(core.AppendFileName + "-bak")  // ignore error
 	// kill all followers connections
 	for conn := range server.aofconnM {
 		conn.Close()

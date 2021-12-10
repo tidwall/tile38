74a23030f21ada36eacc42d2b77731b2518a6d78
Mike Poindexter <mpoindexter@housecanary.com>
2021-12-10 08:56:24 -0800

    Fix key ordering failures

--- tests/json_test.go
+++ tests/json_test.go
@@ -1,6 +1,11 @@
 package tests
 
-import "testing"
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 func subTestJSON(t *testing.T, mc *mockServer) {
 	runStep(t, mc, "basic", json_JSET_basic_test)
@@ -11,16 +16,16 @@ func subTestJSON(t *testing.T, mc *mockServer) {
 func json_JSET_basic_test(mc *mockServer) error {
 	return mc.DoBatch([][]interface{}{
 		{"JSET", "mykey", "myid1", "hello", "world"}, {"OK"},
-		{"JGET", "mykey", "myid1"}, {`{"hello":"world"}`},
+		{"JGET", "mykey", "myid1"}, {jsonEqual(`{"hello":"world"}`)},
 		{"JSET", "mykey", "myid1", "hello", "planet"}, {"OK"},
-		{"JGET", "mykey", "myid1"}, {`{"hello":"planet"}`},
+		{"JGET", "mykey", "myid1"}, {jsonEqual(`{"hello":"planet"}`)},
 		{"JSET", "mykey", "myid1", "user.name.last", "tom"}, {"OK"},
 		{"JSET", "mykey", "myid1", "user.name.first", "andrew"}, {"OK"},
-		{"JGET", "mykey", "myid1"}, {`{"user":{"name":{"first":"andrew","last":"tom"}},"hello":"planet"}`},
+		{"JGET", "mykey", "myid1"}, {jsonEqual(`{"user":{"name":{"first":"andrew","last":"tom"}},"hello":"planet"}`)},
 		{"JDEL", "mykey", "myid1", "user.name.last"}, {1},
-		{"JGET", "mykey", "myid1"}, {`{"user":{"name":{"first":"andrew"}},"hello":"planet"}`},
+		{"JGET", "mykey", "myid1"}, {jsonEqual(`{"user":{"name":{"first":"andrew"}},"hello":"planet"}`)},
 		{"JDEL", "mykey", "myid1", "user.name.last"}, {0},
-		{"JGET", "mykey", "myid1"}, {`{"user":{"name":{"first":"andrew"}},"hello":"planet"}`},
+		{"JGET", "mykey", "myid1"}, {jsonEqual(`{"user":{"name":{"first":"andrew"}},"hello":"planet"}`)},
 		{"JDEL", "mykey2", "myid1", "user.name.last"}, {0},
 	})
 }
@@ -28,16 +33,16 @@ func json_JSET_basic_test(mc *mockServer) error {
 func json_JSET_geojson_test(mc *mockServer) error {
 	return mc.DoBatch([][]interface{}{
 		{"SET", "mykey", "myid1", "POINT", 33, -115}, {"OK"},
-		{"JGET", "mykey", "myid1"}, {`{"type":"Point","coordinates":[-115,33]}`},
+		{"JGET", "mykey", "myid1"}, {jsonEqual(`{"type":"Point","coordinates":[-115,33]}`)},
 		{"JSET", "mykey", "myid1", "coordinates.1", 44}, {"OK"},
-		{"JGET", "mykey", "myid1"}, {`{"type":"Point","coordinates":[-115,44]}`},
+		{"JGET", "mykey", "myid1"}, {jsonEqual(`{"type":"Point","coordinates":[-115,44]}`)},
 		{"SET", "mykey", "myid1", "OBJECT", `{"type":"Feature","geometry":{"type":"Point","coordinates":[-115,44]}}`}, {"OK"},
-		{"JGET", "mykey", "myid1"}, {`{"type":"Feature","geometry":{"type":"Point","coordinates":[-115,44]},"properties":{}}`},
+		{"JGET", "mykey", "myid1"}, {jsonEqual(`{"type":"Feature","geometry":{"type":"Point","coordinates":[-115,44]},"properties":{}}`)},
 		{"JGET", "mykey", "myid1", "geometry.type"}, {"Point"},
 		{"JSET", "mykey", "myid1", "properties.tags.-1", "southwest"}, {"OK"},
 		{"JSET", "mykey", "myid1", "properties.tags.-1", "united states"}, {"OK"},
 		{"JSET", "mykey", "myid1", "properties.tags.-1", "hot"}, {"OK"},
-		{"JGET", "mykey", "myid1"}, {`{"type":"Feature","geometry":{"type":"Point","coordinates":[-115,44]},"properties":{"tags":["southwest","united states","hot"]}}`},
+		{"JGET", "mykey", "myid1"}, {jsonEqual(`{"type":"Feature","geometry":{"type":"Point","coordinates":[-115,44]},"properties":{"tags":["southwest","united states","hot"]}}`)},
 		{"JDEL", "mykey", "myid1", "type"}, {"ERR missing type"},
 	})
 }
@@ -45,12 +50,31 @@ func json_JSET_geojson_test(mc *mockServer) error {
 func json_JSET_number_test(mc *mockServer) error {
 	return mc.DoBatch([][]interface{}{
 		{"JSET", "mykey", "myid1", "hello", "0"}, {"OK"},
-		{"JGET", "mykey", "myid1"}, {`{"hello":0}`},
+		{"JGET", "mykey", "myid1"}, {jsonEqual(`{"hello":0}`)},
 		{"JSET", "mykey", "myid1", "hello", "0123"}, {"OK"},
-		{"JGET", "mykey", "myid1"}, {`{"hello":"0123"}`},
+		{"JGET", "mykey", "myid1"}, {jsonEqual(`{"hello":"0123"}`)},
 		{"JSET", "mykey", "myid1", "hello", "3.14"}, {"OK"},
-		{"JGET", "mykey", "myid1"}, {`{"hello":3.14}`},
+		{"JGET", "mykey", "myid1"}, {jsonEqual(`{"hello":3.14}`)},
 		{"JSET", "mykey", "myid1", "hello", "1.0e10"}, {"OK"},
-		{"JGET", "mykey", "myid1"}, {`{"hello":1.0e10}`},
+		{"JGET", "mykey", "myid1"}, {jsonEqual(`{"hello":1.0e10}`)},
 	})
+}
+
+func jsonEqual(in string) func(v interface{}) (resp, expect interface{}) {
+	return func(v interface{}) (resp, expect interface{}) {
+		var want interface{}
+		var got interface{}
+		err := json.Unmarshal([]byte(in), &want)
+		if err != nil {
+			panic(err)
+		}
+		err = json.Unmarshal([]byte(fmt.Sprint(v)), &got)
+		if err != nil {
+			panic(err)
+		}
+		if reflect.DeepEqual(want, got) {
+			return v, v
+		}
+		return v, in
+	}
 }
